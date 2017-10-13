ebff415247e14593cdfb365f09bf0f0ae5024e20
Harvey Zack <laozhaochaguan@hotmail.com>
2017-10-13 18:08:53 +0800

    update

--- chrome/src/js/options/index.js
+++ chrome/src/js/options/index.js
@@ -1,2 +1,21 @@
 import 'normalize.css';
 import '../../styles/options.scss';
+import { default_url } from '../../config';
+
+window.onload = function() {
+  chrome.storage.local.get('snippetsUrl', storage => {
+    const inputDom = document.querySelector('.input');
+    const btnDom = document.querySelector('.btn');
+    inputDom.value = storage.snippetsUrl ? storage.snippetsUrl : default_url;
+    btnDom.addEventListener('click', function(e) {
+      chrome.storage.local.set(
+        {
+          snippetsUrl: inputDom.value
+        },
+        () => {
+          alert('Saved Successfully!!!');
+        }
+      );
+    });
+  });
+};
