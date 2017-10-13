d45379772f377d02f07c6ebfeb6cb9d0baf08abc
Harvey Zack <laozhaochaguan@hotmail.com>
2017-10-13 18:14:02 +0800

    update

--- chrome/src/js/options/index.js
+++ chrome/src/js/options/index.js
@@ -4,10 +4,15 @@ import { default_url } from '../../config';
 
 window.onload = function() {
   chrome.storage.local.get('snippetsUrl', storage => {
+    const urlExp = /(http[s]?)\:\/\/([\w\-_]+(\.[\w\-_]+){0,5}(\:\d+)?)((\/[\w\.\-_]+\/?)*)\/?(\?([\w\._\-]+\=[\w\.\-_\s+%]*\&?)*)?(\#[\w_\/\-]*)?/gi;
     const inputDom = document.querySelector('.input');
     const btnDom = document.querySelector('.btn');
     inputDom.value = storage.snippetsUrl ? storage.snippetsUrl : default_url;
     btnDom.addEventListener('click', function(e) {
+      if (!urlExp.test(inputDom.value)) {
+        alert('Please enter the correct URL!!!');
+        return;
+      }
       chrome.storage.local.set(
         {
           snippetsUrl: inputDom.value
